8703c5bb759bc485a30cd7ae021b107e8655dc32
rangora <jaeseungkim94@gmail.com>
2024-01-01 15:41:15 +0900

    image 클래스 추가

---
+++ src/Render/Image.cpp
@@ -0,0 +1,69 @@
+﻿#include "Image.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+namespace sb
+{
+    UPtr<Image> Image::Load(const std::string& in_filePath)
+    {
+        auto image = UPtr<Image>(new Image());
+        return image->LoadWithStb(in_filePath) ? std::move(image) : nullptr;
+    }
+
+    UPtr<Image> Image::Create(int32 in_width, int32 in_height, int32 in_channelCount)
+    {
+        auto image = UPtr<Image>(new Image());
+        return image->Allocate(in_width, in_height, in_channelCount) ? std::move(image) : nullptr;
+    }
+
+    Image::~Image()
+    {
+        if (m_data)
+        {
+            stbi_image_free(m_data);
+        }
+    }
+   
+    void Image::SetCheckImage(int32 in_grid_x, int32 in_grid_y)
+    {
+        for (int j = 0; j < m_height; j++)
+        {
+            for (int i = 0; i < m_width; i++)
+            {
+                int pos = (j * m_width + i) * m_channelCount;
+                bool even = ((i / in_grid_x) + (j / in_grid_y)) % 2 == 0; // 한 줄에 4개의 픽셀을 차지하는 체커보드를 만들기 위함.
+                uint8_t value = even ? 255 : 0;
+                for (int k = 0; k < m_channelCount; k++)
+                {
+                    m_data[pos + k] = value;
+                }
+                if (m_channelCount > 3)
+                {
+                    m_data[3] = 255; // alpha값이 0이면 투명해지기 때문에 명시적으로 255를 대입.
+                }
+            }
+        }
+    }
+
+    bool Image::LoadWithStb(const std::string& in_filePath)
+    {
+        stbi_set_flip_vertically_on_load(true);
+        m_data = stbi_load(in_filePath.c_str(), &m_width, &m_height, &m_channelCount, 0);
+        if (!m_data)
+        {
+            // LOG!
+            return false;
+        }
+        return true;
+    }
+  
+    bool Image::Allocate(int32 in_width, int32 in_height, int32 in_channelCount)
+    {
+        m_width = in_width;
+        m_height = in_height;
+        m_channelCount = in_channelCount;
+        m_data = (uint8*)malloc(m_width * m_height * m_channelCount);
+        return m_data ? true : false;
+    }
+} // namespace sb
---
+++ src/Render/Image.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Core/Common.h"
+
+namespace sb
+{
+    class Image
+    {
+        public:
+            static UPtr<Image> Load(const std::string& in_filePath);
+            static UPtr<Image> Create(int32 in_width, int32 in_height, int32 in_channelCount = 4);
+            ~Image();
+
+            const uint8* GetData() const { return m_data; }
+            int32 GetWidth() const { return m_width; }
+            int32 GetHeight() const { return m_height; }
+            int32 GetChannelCount() const { return m_channelCount; }
+
+            void SetCheckImage(int32 in_grid_x, int32 in_grid_y); // SAMPLE // 
+
+        private:
+            Image() = default;
+            bool LoadWithStb(const std::string& in_filePath);
+            bool Allocate(int32 in_width, int32 in_height, int32 in_channelCount);
+            
+            int32 m_width = 0;
+            int32 m_height = 0;
+            int32 m_channelCount = 0;
+            uint8* m_data = nullptr;
+    };
+} // namespace sb
