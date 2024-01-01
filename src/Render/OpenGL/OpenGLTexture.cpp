460ce87a48025e811cf5e5c91de5c655688e9c51
rangora <jaeseungkim94@gmail.com>
2024-01-01 15:58:58 +0900

    OpenGLTextrue class 추가

---
+++ src/Render/OpenGL/OpenGLTexture.cpp
@@ -0,0 +1,71 @@
+﻿#include "OpenGLTexture.h"
+
+#include <glad/glad.h>
+
+namespace sb
+{
+    UPtr<OpenGLTexture> OpenGLTexture::CreateTextureFromImage(const Image* in_image)
+    {
+        auto texture = UPtr<OpenGLTexture>(new OpenGLTexture());
+        texture->CreateTexture();
+        texture->SetTextureFromImage(in_image);
+        return std::move(texture);
+    }
+
+    OpenGLTexture::~OpenGLTexture()
+    {
+        if (m_texture)
+        {
+            glDeleteTextures(1, &m_texture);
+        }
+    }
+    
+    void OpenGLTexture::Bind() const
+    {
+        glBindTexture(GL_TEXTURE_2D, m_texture);
+    }
+    
+    void OpenGLTexture::SetFilter(int32 in_minFilter, int32 in_magFilter) const
+    {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, in_minFilter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, in_magFilter);
+    }
+    
+    void OpenGLTexture::SetWrap(uint32 in_sWarp, uint32 in_tWrap)
+    {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, in_sWarp);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, in_tWrap);
+    }
+    
+    void OpenGLTexture::CreateTexture()
+    {
+        glGenTextures(1, &m_texture);
+        Bind();
+        SetFilter(GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+        SetWrap(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
+    }
+    
+    void OpenGLTexture::SetTextureFromImage(const Image* in_image)
+    {
+        GLenum format = GL_RGBA;
+        switch (in_image->GetChannelCount())
+        {
+            default:
+                break;
+            case 1:
+                format = GL_RED;
+                break;
+            case 2:
+                format = GL_RG;
+                break;
+            case 3:
+                format = GL_RGB;
+                break;
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, in_image->GetWidth(), in_image->GetHeight(), 0, format, GL_UNSIGNED_BYTE,
+                     in_image->GetData());
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+} // namespace sb
---
+++ src/Render/OpenGL/OpenGLTexture.h
@@ -0,0 +1,23 @@
+﻿#define pragma once
+
+#include "Render/Image.h"
+
+namespace sb
+{
+    class OpenGLTexture
+    {
+        static UPtr<OpenGLTexture> CreateTextureFromImage(const Image* in_image);
+        ~OpenGLTexture();
+
+        void Bind() const; // override?
+        void SetFilter(int32 in_minFilter, int32 in_magFilter) const;
+        void SetWrap(uint32 in_sWarp, uint32 in_tWrap);
+
+    private:
+        OpenGLTexture() = default;
+        void CreateTexture();
+        void SetTextureFromImage(const Image* in_image);
+
+        int32 m_texture = 0;
+    };
+} // namespace sb
