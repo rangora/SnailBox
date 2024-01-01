2be8d20c32c524f959934442e0d15595c12de00e
rangora <jaeseungkim94@gmail.com>
2024-01-01 15:59:36 +0900

    OpenGLBuffer class 추가

---
+++ src/Render/OpenGL/OpenGLBuffer.cpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Core/Common.h"
+
+namespace sb
+{
+    class OpenGLVertexBuffer
+    {
+    public:
+        ~OpenGLVertexBuffer();
+        static UPtr<OpenGLVertexBuffer> Create();
+
+        int32 Get() const { return m_vertexArrayObject; }
+
+        void Bind() const; // override?
+        void SetAttribute(int32 in_attribIndex, int in_count, int32 in_type, bool in_normalized, size_t in_stride,
+                          uint64_t in_offset) const; // override?
+
+private:
+    OpenGLVertexBuffer() = default;
+    void Init();
+
+    int32 m_vertexArrayObject = 0;
+    };
+} // namespace sb
---
+++ src/Render/OpenGL/OpenGLBuffer.h
@@ -0,0 +1,39 @@
+﻿#include "OpenGLBuffer.h"
+
+#include <glad/glad.h>
+
+namespace sb
+{
+    inline OpenGLVertexBuffer::~OpenGLVertexBuffer()
+    {
+        if (m_vertexArrayObject)
+        {
+            glDeleteVertexArrays(1, &m_vertexArrayObject);
+        }
+    }
+
+    inline UPtr<OpenGLVertexBuffer> OpenGLVertexBuffer::Create()
+    {
+        auto VertexBuffer = UPtr<OpenGLVertexBuffer>(new OpenGLVertexBuffer());
+        VertexBuffer->Init();
+        return std::move(VertexBuffer);
+    }
+
+    void OpenGLVertexBuffer::Bind() const
+    {
+        glBindVertexArray(m_vertexArrayObject);
+    }
+
+    void OpenGLVertexBuffer::SetAttribute(int32 in_attribIndex, int in_count, int32 in_type, bool in_normalized,
+                                          size_t in_stride, uint64_t in_offset) const
+    {
+        glEnableVertexAttribArray(in_attribIndex);
+        glVertexAttribPointer(in_attribIndex, in_count, in_type, in_normalized, in_stride, (const void*)in_offset);
+    }
+
+    void OpenGLVertexBuffer::Init()
+    {
+        glGenVertexArrays(1, &m_vertexArrayObject);
+        Bind();
+    }
+}
