9739e1f67135ba96305ebb5bdc748a660a848b17
rangora <jaeseungkim94@gmail.com>
2024-04-13 16:27:37 +0900

    vector.h 추가

---
+++ src/Core/Math/Vector.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "Core/Platform.h"
+
+namespace sb {
+    template <typename T>
+    struct Vector
+    {
+        struct
+        {
+            T X;
+            T Y;
+            T Z;
+        };
+
+        explicit FORCEINLINE Vector(T in_val);
+        FORCEINLINE Vector(T in_x, T in_y, T in_z);
+
+        FORCEINLINE Vector<T> operator+(const Vector<T>& U) const;
+        FORCEINLINE Vector<T> operator-(const Vector<T>& U) const;
+        FORCEINLINE Vector<T> operator*(const Vector<T>& U) const;
+        FORCEINLINE Vector<T> operator/(const Vector<T>& U) const;
+        FORCEINLINE bool operator==(const Vector<T>& U) const;
+    };
+
+    template <typename T>
+    FORCEINLINE Vector<T>::Vector(T in_val) : X(in_val), Y(in_val), Z(in_val) {}
+
+    template <typename T>
+    FORCEINLINE Vector<T> Vector<T>::operator+(const Vector<T>& U) const
+    {
+        return TVector<T>(X + U.X, Y + U.Y, Z + U.Z);
+    }
+
+    template <typename T>
+    FORCEINLINE Vector<T> Vector<T>::operator-(const Vector<T>& U) const
+    {
+        return TVector<T>(X - U.X, Y - U.Y, Z - U.Z);
+    }
+
+    template <typename T>
+    FORCEINLINE Vector<T> Vector<T>::operator*(const Vector<T>& U) const
+    {
+        return TVector<T>(X * U.X, Y * U.Y, Z * U.Z);
+    }
+
+    template <typename T>
+    FORCEINLINE Vector<T> Vector<T>::operator/(const Vector<T>& U) const
+    {
+        return TVector<T>(X / U.X, Y / U.Y, Z / U.Z);
+    }
+
+    template <typename T>
+    FORCEINLINE bool Vector<T>::operator==(const Vector<T>& U) const
+    {
+        return X == U.X && Y == U.Y && Z == U.Z;
+    }
+}
--- src/Core/Platform.h
+++ src/Core/Platform.h
@@ -2,4 +2,7 @@
     #ifdef _WIN64
         #define ON_PLATFORM_WINDOW
     #endif
+    #if defined(_MSC_VER)
+        #define FORCEINLINE __forceinline
+    #endif
 #endif
