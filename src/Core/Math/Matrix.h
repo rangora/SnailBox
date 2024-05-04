78402fdd67cbfdeee259669fc08f93f152a070ac
rangora <jaeseungkim94@gmail.com>
2024-05-04 14:55:00 +0900

    메트릭스 수정

--- src/Core/Math/Matrix.h
+++ src/Core/Math/Matrix.h
@@ -2,34 +2,38 @@
 
 #include "Core/Platform.h"
 #include "Vector.h"
+#include "Quat.h"
+#include <glm/glm.hpp>
 
 namespace sb
 {
     template <typename T>
-    struct alignas(16) Matrix
+    struct alignas(16) AlignMatrix
     {
         alignas(16) T M[4][4];
 
         void SetIdentity();
 
-        FORCEINLINE Matrix();
-        FORCEINLINE Matrix(const Vector3d& inX, const Vector3d& inY, const Vector3d& inZ, const Vector3d& inW);
+        FORCEINLINE AlignMatrix();
+        FORCEINLINE AlignMatrix(const Vector3d& inX, const Vector3d& inY, const Vector3d& inZ, const Vector3d& inW);
 
-        FORCEINLINE Matrix<T> operator+(const Matrix<T>& U) const;
-        FORCEINLINE Matrix<T> operator-(const Matrix<T>& U) const;
-        FORCEINLINE Matrix<T> operator*(const Matrix<T>& U) const;
-        FORCEINLINE Matrix<T> operator/(const Matrix<T>& U) const;
-        FORCEINLINE bool operator==(const Matrix<T>& U) const;
+        FORCEINLINE AlignMatrix<T> operator+(const AlignMatrix<T>& U) const;
+        FORCEINLINE AlignMatrix<T> operator-(const AlignMatrix<T>& U) const;
+        FORCEINLINE AlignMatrix<T> operator*(const AlignMatrix<T>& U) const;
+        FORCEINLINE AlignMatrix<T> operator/(const AlignMatrix<T>& U) const;
+        FORCEINLINE bool operator==(const AlignMatrix<T>& U) const;
 
         template<typename U>
-        FORCEINLINE Matrix<T>& operator=(const Matrix<U>& in_other);
+        FORCEINLINE AlignMatrix<T>& operator=(const AlignMatrix<U>& in_other);
+
+        glm::mat4 ToGlmMat4();
     };
 
     template <typename T>
-    FORCEINLINE Matrix<T>::Matrix() {}
+    FORCEINLINE AlignMatrix<T>::AlignMatrix() {}
 
     template <typename T>
-    FORCEINLINE Matrix<T>::Matrix(const Vector3d& inX, const Vector3d& inY, const Vector3d& inZ, const Vector3d& inW)
+    FORCEINLINE AlignMatrix<T>::AlignMatrix(const Vector3d& inX, const Vector3d& inY, const Vector3d& inZ, const Vector3d& inW)
     {
         M[0][0] = inX.X; M[0][1] = inX.Y;  M[0][2] = inX.Z;  M[0][3] = 0.0;
         M[1][0] = inY.X; M[1][1] = inY.Y;  M[1][2] = inY.Z;  M[1][3] = 0.0;
@@ -38,7 +42,7 @@ namespace sb
     }
 
     template <typename T>
-    FORCEINLINE Matrix<T> Matrix<T>::operator+(const Matrix<T>& U) const
+    FORCEINLINE AlignMatrix<T> AlignMatrix<T>::operator+(const AlignMatrix<T>& U) const
     {
         M[0][0] += U.M[0][0]; M[0][1] += U.M[0][1];  M[0][2] += U.M[0][2];
         M[1][0] += U.M[1][0]; M[1][1] += U.M[1][1];  M[1][2] += U.M[1][2];
@@ -47,7 +51,7 @@ namespace sb
     }
 
     template <typename T>
-    FORCEINLINE Matrix<T> Matrix<T>::operator-(const Matrix<T>& U) const
+    FORCEINLINE AlignMatrix<T> AlignMatrix<T>::operator-(const AlignMatrix<T>& U) const
     {
         M[0][0] -= U.M[0][0]; M[0][1] -= U.M[0][1];  M[0][2] -= U.M[0][2];
         M[1][0] -= U.M[1][0]; M[1][1] -= U.M[1][1];  M[1][2] -= U.M[1][2];
@@ -56,7 +60,7 @@ namespace sb
     }
 
     template <typename T>
-    FORCEINLINE Matrix<T> Matrix<T>::operator*(const Matrix<T>& U) const
+    FORCEINLINE AlignMatrix<T> AlignMatrix<T>::operator*(const AlignMatrix<T>& U) const
     {
         for (int i = 0; i < 4; i++)
         {
@@ -74,7 +78,7 @@ namespace sb
     }
 
     template <typename T>
-    FORCEINLINE Matrix<T> Matrix<T>::operator/(const Matrix<T>& U) const
+    FORCEINLINE AlignMatrix<T> AlignMatrix<T>::operator/(const AlignMatrix<T>& U) const
     {
         for (int i = 0; i < 4; i++)
         {
@@ -92,7 +96,7 @@ namespace sb
     }
 
     template <typename T>
-    FORCEINLINE bool Matrix<T>::operator==(const Matrix<T>& U) const
+    FORCEINLINE bool AlignMatrix<T>::operator==(const AlignMatrix<T>& U) const
     {
         (M[0][0] == U.M[0][0] && M[0][1] == U.M[0][1] && M[0][2] == U.M[0][2] && M[1][0] == U.M[1][0] &&
          M[1][1] == U.M[1][1] && M[1][2] == U.M[1][2] && M[2][0] == U.M[2][0] && M[2][1] == U.M[2][1] &&
@@ -103,7 +107,7 @@ namespace sb
 
     template <typename T>
     template <typename U>
-    FORCEINLINE Matrix<T>& Matrix<T>::operator=(const Matrix<U>& in_other)
+    FORCEINLINE AlignMatrix<T>& AlignMatrix<T>::operator=(const AlignMatrix<U>& in_other)
     {
         M[0][0] = U.M[0][0]; M[0][1] = U.M[0][1];  M[0][2] = U.M[0][2]; M[0][3] = U.M[0][3];
         M[1][0] = U.M[1][0]; M[1][1] = U.M[1][1];  M[1][2] = U.M[1][2]; M[1][3] = U.M[1][3];
@@ -114,11 +118,24 @@ namespace sb
     }
 
     template <typename T>
-    void Matrix<T>::SetIdentity()
+    void AlignMatrix<T>::SetIdentity()
     {
         M[0][0] = 1; M[0][1] = 0;  M[0][2] = 0;  M[0][3] = 0.0;
         M[1][0] = 0; M[1][1] = 1;  M[1][2] = 0;  M[1][3] = 0.0;
         M[2][0] = 0; M[2][1] = 0;  M[2][2] = 1;  M[2][3] = 0.0;
         M[3][0] = 0; M[3][1] = 0;  M[3][2] = 0;  M[3][3] = 1.0;
     }
+
+    template <typename T>
+    glm::mat4 AlignMatrix<T>::ToGlmMat4()
+    {
+        glm::mat4 ret(1.f);
+        ret[3][0] = M[0][0];
+        ret[3][1] = M[0][1];
+        ret[3][2] = M[0][2];
+
+        return ret;
+    }
+
+    using Matrix = AlignMatrix<double>;
 };
