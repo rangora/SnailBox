473ecec80f8081ba061c3ab43cc112a3b3e713d8
rangora <jaeseungkim94@gmail.com>
2024-06-30 17:54:00 +0900

    frontWindowUI 조금 개선

--- src/Core/FrontWindow.cpp
+++ src/Core/FrontWindow.cpp
@@ -12,16 +12,35 @@ namespace sb
     {
     }
 
+    bool FrontWindow::InitializeFrontWindow()
+    {
+        m_wc = {sizeof(m_wc), CS_CLASSDC,         WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr,
+                nullptr,      L"FrontMenuWindow", nullptr};
+        ::RegisterClassExW(&m_wc);
+
+        m_hwnd = ::CreateWindowW(m_wc.lpszClassName, L"FrontWindow", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr,
+                                 nullptr, m_wc.hInstance, nullptr);
+
+        return true;
+    }
+
+    bool FrontWindow::BindDirect3dDriver()
+    {
+        
+        return false;
+    }
+
     void FrontWindow::ImGuiUpdate()
     {
         ImGui::Begin("FrontWindow", 0);
         ImGui::Text("Hello, world!");
-        static int e = 0;
-        ImGui::RadioButton("radio a", &e, 0);
+        ImGui::RadioButton("DirectX12", &m_driverType, 0);
         ImGui::SameLine();
-        ImGui::RadioButton("radio b", &e, 1);
-        ImGui::SameLine();
-        ImGui::RadioButton("radio c", &e, 2);
+        ImGui::RadioButton("OpenGL", &m_driverType, 1);
+        if (ImGui::Button("Next"))
+        {
+            m_bNext = true;
+        }
         ImGui::End();
     }
 } // namespace sb
--- src/Core/FrontWindow.h
+++ src/Core/FrontWindow.h
@@ -12,6 +12,12 @@ namespace sb
         FrontWindow(const WindowContext& in_windowContext, class Application* in_app);
         virtual ~FrontWindow() {};
 
+        bool InitializeFrontWindow();
+        bool BindDirect3dDriver();
+
         void ImGuiUpdate() final;
+
+        bool m_bNext = false;
+        int32 m_driverType = 0;
     };
 };
